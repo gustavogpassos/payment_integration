2b81136e7c907594782acb7557e5b4bc6182fcda
guzao03 <girardi_gustavo@outlook.com>
2017-06-09 22:04:34 -0300

    Alteração de conta 1

--- boot.php
+++ boot.php
@@ -2,13 +2,14 @@
 
 require "vendor/autoload.php";
 
-/*Braintree_Configuration::environment('sandbox');
+Braintree_Configuration::environment('sandbox');
 Braintree_Configuration::merchantId('bbpvr5hy9y659nyg');
 Braintree_Configuration::publicKey('tdkqh3m2cx3c728j');
-Braintree_Configuration::privateKey('b4cd2051fdd35762771c8ab016869f1c');*/
+Braintree_Configuration::privateKey('b4cd2051fdd35762771c8ab016869f1c');
 
-
+/*
 Braintree_Configuration::environment('sandbox');
 Braintree_Configuration::merchantId('svfbpr8jq9r5pnxt');
 Braintree_Configuration::publicKey('kbmzv3prm6m58g4s');
 Braintree_Configuration::privateKey('f6aa757e5cb91f9d7c938f85fe45066a');
+*/
